41858b9c9573a7f2c8e5bc8c33f57b83c9dc9422
MauricioSts <Mauricio.santos.110@ufrn.edu.br>
2025-03-11 15:23:02 -0300

    aprendendo sobre alinhamentos

--- frase_do_dia/lib/main.dart
+++ frase_do_dia/lib/main.dart
@@ -8,18 +8,18 @@ void main() {
       home: Container(
         //color: Colors.white,
         padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-        margin: EdgeInsets.all(0),
+        margin: EdgeInsets.only(top: 30),
         //margin: EdgeInsets.only(top: 50, left: 50),
         //margin: EdgeInsets.fromLTRB(10, 60, 30, 40),
         decoration: BoxDecoration(
           border: Border.all(width: 3, color: Colors.white),
         ),
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Text("t1"),
-
-            Padding(padding: EdgeInsets.all(30), child: Text("t2")),
-
+            Text("t2"),
             Text("t3"),
             /*Text(
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum posuere rhoncus velit, eget commodo",
