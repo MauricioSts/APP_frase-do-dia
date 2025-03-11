50db240e16edf5547444b6608b6bf3110cc5c8d7
MauricioSts <Mauricio.santos.110@ufrn.edu.br>
2025-03-11 14:12:19 -0300

    aprendendo sobre flatbutton

--- frase_do_dia/lib/main.dart
+++ frase_do_dia/lib/main.dart
@@ -8,18 +8,17 @@ void main() {
         color: Colors.white,
         child: Column(
           children: <Widget>[
-            Text(
-              "lorem ipsum",
-              style: TextStyle(
-                fontSize: 35,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic,
-                letterSpacing: 0,
-                wordSpacing: 0,
-                decoration: TextDecoration.underline,
-                decorationStyle: TextDecorationStyle.solid,
-                decorationColor: Colors.black,
-                color: Colors.black,
+            FloatingActionButton(
+              onPressed: () {
+                print("pressionado");
+              },
+              child: Text(
+                "Clique aqui",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.amber,
+                  decoration: TextDecoration.none,
+                ),
               ),
             ),
           ],
