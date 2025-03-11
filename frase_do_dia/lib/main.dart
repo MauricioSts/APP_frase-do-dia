248ceadc9ddec552030025c4f8fe1a10fb7b355e
MauricioSts <Mauricio.santos.110@ufrn.edu.br>
2025-03-11 15:04:37 -0300

    aprendendo espaçamentos

--- frase_do_dia/lib/main.dart
+++ frase_do_dia/lib/main.dart
@@ -3,24 +3,28 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      title: "Frase do dia",
+      //debugShowCheckedModeBanner: false,
+      title: "Frases do dia",
       home: Container(
-        color: Colors.white,
-        child: Column(
+        //color: Colors.white,
+        padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+        margin: EdgeInsets.all(0),
+        //margin: EdgeInsets.only(top: 50, left: 50),
+        //margin: EdgeInsets.fromLTRB(10, 60, 30, 40),
+        decoration: BoxDecoration(
+          border: Border.all(width: 3, color: Colors.white),
+        ),
+        child: Row(
           children: <Widget>[
-            FloatingActionButton(
-              onPressed: () {
-                print("pressionado");
-              },
-              child: Text(
-                "Clique aqui",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.amber,
-                  decoration: TextDecoration.none,
-                ),
-              ),
-            ),
+            Text("t1"),
+
+            Padding(padding: EdgeInsets.all(30), child: Text("t2")),
+
+            Text("t3"),
+            /*Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum posuere rhoncus velit, eget commodo",
+              textAlign: TextAlign.justify,
+          )*/
           ],
         ),
       ),
