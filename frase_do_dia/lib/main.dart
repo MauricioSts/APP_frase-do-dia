fca9b95bafed7cca3a88263ef3e26c155b5bb126
MauricioSts <mauricio.santos.110@ufrn.edu.br>
2025-03-11 18:55:00 -0300

    Aprendendo sobre o scaffold

--- frase_do_dia/lib/main.dart
+++ frase_do_dia/lib/main.dart
@@ -3,18 +3,33 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      //debugShowCheckedModeBanner: false,
-      title: "Frases do dia",
-      home: Container(
-        //color: Colors.white,
-        padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-        margin: EdgeInsets.only(top: 30),
-        //margin: EdgeInsets.only(top: 50, left: 50),
-        //margin: EdgeInsets.fromLTRB(10, 60, 30, 40),
-        decoration: BoxDecoration(
-          border: Border.all(width: 3, color: Colors.white),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text(
+            "Aplicativo",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
         ),
-        child: Image.asset("images/terra.jpg", fit: BoxFit.cover),
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: Text(
+            "conteudo principal do app",
+            style: TextStyle(fontSize: 40),
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          padding: EdgeInsets.all(16),
+          color: Colors.amber,
+          child: Row(
+            children: <Widget>[
+              Text("texto 1"),
+              Text("texto 2"),
+              Text("texto 3"),
+            ],
+          ),
+        ),
       ),
     ),
   );
