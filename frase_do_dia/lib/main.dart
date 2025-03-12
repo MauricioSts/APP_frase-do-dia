382e519172ad795fc105b7e769658f28ebf078ca
MauricioSts <Mauricio.santos.110@ufrn.edu.br>
2025-03-12 14:53:39 -0300

    App frase do dia finalizado

--- frase_do_dia/lib/main.dart
+++ frase_do_dia/lib/main.dart
@@ -1,43 +1,70 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomeState()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: homewid()));
 }
 
-class HomeState extends StatefulWidget {
-  const HomeState({super.key});
+class homewid extends StatefulWidget {
+  const homewid({super.key});
 
   @override
-  State<HomeState> createState() => _MyWidgetState();
+  State<homewid> createState() => _homewidState();
 }
 
-class _MyWidgetState extends State<HomeState> {
-  var _texto = "ola";
+class _homewidState extends State<homewid> {
+  List<String> _frases = [
+    "A vida é como um livro, e cada dia é uma nova página. Escreva algo incrível!",
+    "Não importa a velocidade, o importante é nunca parar de caminhar rumo aos seus sonhos.",
+    "As dificuldades preparam pessoas comuns para destinos extraordinários.",
+    "Seja a energia que você quer atrair. O universo responde à sua vibração.",
+    "Cada novo dia é uma nova chance para recomeçar e fazer melhor do que ontem.",
+  ];
+
+  var fraseAtual = "clique no botão para gerar uma nova frase";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text("Frases do dia", style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.green,
-        title: Text(
-          "Aplicativo",
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
       ),
-      body: Container(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            FloatingActionButton(
-              onPressed: () {
-                setState(() {
-                  _texto = "mau";
-                });
-              },
-              child: Text("clique aqui", style: TextStyle(fontSize: 20)),
-            ),
-            Text(_texto, style: TextStyle(fontSize: 20)),
-          ],
+
+      body: Center(
+        child: Container(
+          // width: double.infinity,
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset("images/logo.png"),
+              Text(
+                fraseAtual,
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontSize: 20),
+              ),
+
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.green),
+                onPressed: () {
+                  setState(() {
+                    fraseAtual = _frases[Random().nextInt(_frases.length)];
+                  });
+                },
+                child: Text(
+                  "Gerar nova frase",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
