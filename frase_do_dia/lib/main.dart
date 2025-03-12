542206f314e674ea9e0fba20559e6c4e2134fb3d
MauricioSts <mauricio.santos.110@ufrn.edu.br>
2025-03-11 21:24:12 -0300

    entendendo a diferença entre stateless X statefull

--- frase_do_dia/lib/main.dart
+++ frase_do_dia/lib/main.dart
@@ -1,36 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: Text(
-            "Aplicativo",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(16),
-          child: Text(
-            "conteudo principal do app",
-            style: TextStyle(fontSize: 40),
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          padding: EdgeInsets.all(16),
-          color: Colors.amber,
-          child: Row(
-            children: <Widget>[
-              Text("texto 1"),
-              Text("texto 2"),
-              Text("texto 3"),
-            ],
-          ),
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomeState()));
+}
+
+class HomeState extends StatefulWidget {
+  const HomeState({super.key});
+
+  @override
+  State<HomeState> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<HomeState> {
+  var _texto = "ola";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text(
+          "Aplicativo",
+          style: TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
-    ),
-  );
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  _texto = "mau";
+                });
+              },
+              child: Text("clique aqui", style: TextStyle(fontSize: 20)),
+            ),
+            Text(_texto, style: TextStyle(fontSize: 20)),
+          ],
+        ),
+      ),
+    );
+  }
 }
